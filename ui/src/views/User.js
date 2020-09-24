bc243a62df163c34ad95a73ba2adc583276f2627
itays123 <itays2005@gmail.com>
2020-09-24 17:30:07 +0300

    redirecting from user page to profile

--- ui/src/views/User.js
+++ ui/src/views/User.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Redirect, useParams } from 'react-router-dom';
 import PostList from '../components/PostList/PostList';
 import UserCard from '../components/UserCard/UserCard';
+import useProfile from '../hooks/useProfile';
 import useUser from '../hooks/useUser';
+
+const RedirectOnProfile = ({ currentUser, children }) => {
+  const { _id } = useProfile();
+  if (currentUser === _id) return <Redirect to="/profile" />;
+  else return <>{children}</>;
+};
 
 const User = () => {
   const { id } = useParams();
   const user = useUser(id);
   return (
-    <div className="user route">
-      {!user.loading && user.isFound && (
-        <>
-          <UserCard {...user} id={id} />
-          <PostList posts={user.created} />
-        </>
-      )}
-    </div>
+    <RedirectOnProfile currentUser={id}>
+      <div className="user route">
+        {!user.loading && user.isFound && (
+          <>
+            <UserCard {...user} id={id} />
+            <PostList posts={user.created} />
+          </>
+        )}
+      </div>
+    </RedirectOnProfile>
   );
 };
 
