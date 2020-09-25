a1f4db139c5f40006a74d664273ac79c6283b0d7
itays123 <itays2005@gmail.com>
2020-09-25 13:12:57 +0300

    posting text from the ui

--- ui/src/components/CreatePost/CreatePost.js
+++ ui/src/components/CreatePost/CreatePost.js
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CreatePost.css';
 import useProfile from '../../hooks/useProfile';
 import ProfilePic from '../ProfilePic/ProfilePic';
 import ResizeableTextArea from '../ResizeableTextArea/ResizeableTextArea';
+import { usePost } from '../../hooks/usePost';
 
 const CreatePost = () => {
   const { avatarUrl, name } = useProfile();
+  const [content, setContent] = useState('');
+  const { post } = usePost();
   const placeHolder = `What's on your mind, ${name ? name.split(' ')[0] : ''}?`;
   return (
     <div className="create-post">
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          post([], content);
+        }}
+      >
         <div className="text flex a-stretch">
           <div className="profile">
             <ProfilePic url={avatarUrl} />
           </div>
           <div className="rta-container">
-            <ResizeableTextArea placeholder={placeHolder} />
+            <ResizeableTextArea
+              onChange={c => setContent(c)}
+              placeholder={placeHolder}
+            />
           </div>
+        </div>
+        <div className="flex a-center j-center">
+          <button>Post</button>
         </div>
       </form>
     </div>
--- ui/src/hooks/usePost.js
+++ ui/src/hooks/usePost.js
@@ -3,28 +3,26 @@ import { useMutation } from '@apollo/client';
 import { useEffect, useState } from 'react';
 
 const POST = gql`
-  mutation PostIt($gallery: [String] = [], $content: String = '') {
-      NewPost(content: $content, gallery: $gallery) {
-          _id
-      }
+  mutation PostIt($gallery: [String] = [], $content: String = "") {
+    NewPost(content: $content, gallery: $gallery) {
+      _id
+    }
   }
 `;
 
 /**
  * contains all functionality and error handling to post
- * @returns {{ post(gallery: string[], content: string, callback: Function = () => {}): void }}
+ * @returns {{ post(gallery: string[], content: string): void }}
  */
 export function usePost() {
-  const [callback, setCallback] = useState(() => () => {});
   const [PostIt, { error, data }] = useMutation(POST);
   useEffect(() => {
     if (!error && data?.NewPost?._id) {
-      callback(data.NewPost._id);
+      console.log(data);
     }
   }, [data, error]);
   return {
-    post(gallery, content, callback) {
-      setCallback(callback);
+    post(gallery, content) {
       PostIt({ variables: { gallery, content } });
     },
   };
