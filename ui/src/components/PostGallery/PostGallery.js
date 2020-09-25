2d6bdadd7aa894002d740aba61acf48bd8fc078b
itays123 <itays2005@gmail.com>
2020-09-25 11:48:12 +0300

    styling the gallery component

--- ui/src/components/PostGallery/PostGallery.js
+++ ui/src/components/PostGallery/PostGallery.js
@@ -7,7 +7,7 @@ import './PostGallery.css';
  */
 const PostGallery = ({ gallery }) => {
   return (
-    <div className="gallery flex">
+    <div className="gallery">
       {gallery.map((url, i) => (
         <img src={url} key={i} alt="" height={150} width="auto" />
       ))}
